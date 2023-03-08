152ae85da18b2e46a99da97cb3b52c48b646d3df
Antoine BLUMENROEDER <antoine.blumenroeder@epita.fr>
2023-03-08 18:34:52 +0100

    cleaning up

--- game/Assets/Scripts/Fighting/Player.cs
+++ game/Assets/Scripts/Fighting/Player.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using Items;
 using UnityEngine;
-using Object = System.Object;
 
 namespace Fighting
 {
--- game/Assets/Scripts/Items/UIItem.cs
+++ game/Assets/Scripts/Items/UIItem.cs
@@ -1,7 +1,6 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Items
